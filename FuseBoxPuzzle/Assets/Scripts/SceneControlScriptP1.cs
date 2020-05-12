c3fac024f8a37b82c5e2f7418a53defbef112b1a
Tom Blanchard <tomkb@uw.edu>
2020-05-12 17:45:15 -0400

    Fuse Box Puzzle Prototype
    Should be working puzzle sets for both players. Not the most modular but I don't see us using this type of puzzle again - therefore there is some use of statics and whatnot.

---
+++ FuseBoxPuzzle/Assets/Scripts/SceneControlScriptP1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneControlScriptP1 : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject unlockText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unlockText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(CableScript.totalTile1 + ", " + CableScript.totalTile2 + ", " + CableScript.totalTile3);
+        if (CableScript.totalTile1 == 35 && CableScript.totalTile2 == 23
+            && CableScript.totalTile3 == 27)
+        {
+            unlockText.SetActive(true);
+        }
+    }
+}
---
+++ FuseBoxPuzzle/Assets/Scripts/SceneControlScriptP2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneControlScriptP2 : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject unlockText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unlockText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(CableScript.totalTile1 + ", " + CableScript.totalTile2 + ", " + CableScript.totalTile3);
+        if (CableScript.totalTile1 == 26 && CableScript.totalTile3 == 29
+            && CableScript.totalTile2 == 32) {
+            unlockText.SetActive(true);
+        }
+    }
+}
