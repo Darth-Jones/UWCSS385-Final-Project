f857e8b16133070b50a8ea6279cb05ae1dea4f0a
lukaszbak <lukaszdbakun@gmail.com>
2020-05-13 17:06:11 -0700

    Updated dark room to have map, UI, and goes to dark on enter, turns bright on leave
    Known issues: Camera shake causes the camera to attach to player, Room does not turn back dark on re entry

---
+++ BlackRoom Concept/Assets/cameraShake.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cameraShake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Shakes the camera and Turns out the lights
+    
+    public GameObject cameraObject;
+    public float shakeLength;
+    public float shakeSize;
+    public Vector3 cameraOrigPos;
+    private cameraFollowScript2 cameraScript;
+    Rigidbody2D rb2d;
+    public bool shake = false;
+    private bool lastShake = false;
+    private float i;
+    private float j;
+    private int frameCounter;
+    private int startShake;
+    private float endShake;
+    public GameObject darkUI;
+    private RawImage image;
+    public Color c;
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        cameraObject = GameObject.FindGameObjectsWithTag("MainCamera")[0];
+        cameraScript = cameraObject.GetComponent<cameraFollowScript2>();
+        darkUI = GameObject.FindGameObjectsWithTag("darkUI")[0];
+        image = darkUI.GetComponent<RawImage>();
+        i = 0;
+        j = 30;
+        frameCounter = 0;
+        c = image.color;
+        c.a = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cameraOrigPos.x > 5) { shake = false;}
+        cameraOrigPos = cameraScript.expectedPos;
+        frameCounter++;
+        if(shake) {
+            startShake = frameCounter;
+            i = 0;
+            j = 60;
+            
+            endShake = startShake + 120 + shakeLength * 60;
+        }
+        if (frameCounter < endShake && startShake < frameCounter) {
+            i++;
+            c.a = c.a + .005f;
+            image.color =  c;
+            cameraScript.rubberBandNess = 1.5f;
+            if ( i < 60) {
+                float xMovement = Random.Range(-shakeSize * i/60, shakeSize * i/60);
+                float yMovement = Random.Range(-shakeSize * i/60, shakeSize * i/60);
+                cameraObject.transform.position = cameraOrigPos + new Vector3(xMovement, yMovement, -10);
+            }
+            else if ( i > 60 && i < shakeLength * 60 + 60) {
+                float xMovement = Random.Range(-shakeSize, shakeSize);
+                float yMovement = Random.Range(-shakeSize, shakeSize);
+                cameraObject.transform.position = cameraOrigPos + new Vector3(xMovement, yMovement, -10);
+            }
+            else {
+                float xMovement = Random.Range(-shakeSize * j/60, shakeSize * j/60);
+                float yMovement = Random.Range(-shakeSize * j/60, shakeSize * j/60);
+                cameraObject.transform.position = cameraOrigPos + new Vector3(xMovement, yMovement, -10);
+                j--;
+            }
+            
+        }
+        Debug.Log("StartShake = " + startShake + " EndShake = " + endShake + " frameCounter = " + frameCounter + " i = " + i + " j = " + j);
+        
+        //lastShake = shake;
+
+        
+        
+    }
+
+
+}
