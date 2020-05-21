7c32d751869bd85f6d38bb14d54d12f45918e682
James Pfleger <james.pfleger@gmail.com>
2020-05-21 11:57:14 -0700

    Working Email System
    Working email for demo. Code is a little spagetti and needs to be fix a bit

---
+++ Final-Project-Game/Assets/Scripts/Email.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using JetBrains.Annotations;
+using UnityEngine.UI;
+
+public class Email 
+{
+
+    public string headerText;
+    public string bodyText;
+    public string hintText;
+    public bool hasHintText;
+
+    public int emailID;
+    public bool questFinished;
+    public GameObject flashingTextCanvas;
+    public GameObject bodyCanvas;
+
+    public Email(int emailID) 
+    {
+
+    
+
+        this.emailID = emailID;
+        string path = Application.dataPath + "/Text/questObj" + emailID + ".txt";
+        Debug.Log(path);
+        string questText = File.ReadAllText(path);
+        Debug.Log(questText);
+        string[] questInfo = questText.Split('|');
+        // int numberOfInt32.TryParse(TextBoxD1.Text, out x);
+        // for (int i = 1; i < int.TryParse(questInfo[0]) + 1; i++)
+        // {
+        headerText = questInfo[0];
+        bodyText = questInfo[1];
+        hasHintText = false;
+        int hintTextValue = int.Parse(questInfo[2]);
+        if (hintTextValue > 0)
+        {
+            hasHintText  = true;
+            hintText = questInfo[3];
+        }
+        else
+        {
+            questFinished = true;
+        }
+
+        
+
+
+        Debug.Log("Asdfa" + headerText);
+        flashingTextCanvas = GameObject.Find("FlashingTextCanvas");
+        bodyCanvas = GameObject.Find("BodyCanvas");
+
+
+        flashingTextCanvas.GetComponentInChildren<TextFlashScript>().Flash("NEW EMAIL");
+    }
+
+    public Email(int emailID, string header, string body, string hintText)
+    {
+        this.emailID = emailID;
+        headerText = header;
+        bodyText = body;
+        hasHintText = true;
+        this.hintText = hintText;
+        flashingTextCanvas = GameObject.Find("FlashingTextCanvas");
+        bodyCanvas = GameObject.Find("BodyCanvas");
+    }
+
+    public Email(int emailID, string header, string body)
+    {
+        this.emailID = emailID;
+        headerText = header;
+        bodyText = body;
+        hasHintText = false;
+        this.hintText = "";
+        flashingTextCanvas = GameObject.Find("FlashingTextCanvas");
+        bodyCanvas = GameObject.Find("BodyCanvas");
+    }
+
+    public bool stepCompleted()
+    {
+      //  Debug.Log("body text here:" + bodyText);
+  //      bodyCanvas.GetComponentInChildren<Text>().text = bodyText;   //GetComponentOfChild<Text>().text = 
+
+        if (hasHintText)
+        {
+            if (!questFinished)
+            {
+                Debug.Log("stepCompleted part");
+                GameObject.Find("Canvas2").transform.GetChild(0).gameObject.SetActive(true);
+                Debug.Log("starting flash");
+                GameObject.Find("FlashingTextCanvas").GetComponent<TextFlashScript>().Flash(hintText);
+                GameObject.Find("EventTracker").GetComponent<EventLog>().newText(hintText, emailID);
+
+                questFinished = true;
+            }
+               
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Final-Project-Game/Assets/Scripts/EmailListControl.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class EmailListControl : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject buttonTemplate;
+
+    public GameObject flashingText;
+    private int startingEmailCount = 6;
+    // Start is called before the first frame update
+
+    private List<GameObject> buttonList;
+
+    private void Start()
+    {
+        //  emailList = new List<Email>();
+        GenStartingEmails();
+    }
+
+    private void GenStartingEmails()
+    {
+        string path = Application.dataPath + "/Text/startingEmails.txt";
+        Debug.Log(path);
+        string questText = File.ReadAllText(path);
+        string[] emailTextSplit = questText.Split('|');
+
+
+        Debug.Log("first button" + emailTextSplit[1]);
+
+        GameObject button = Instantiate(buttonTemplate) as GameObject;
+        button.SetActive(true);
+
+        button.GetComponent<EmailButton>().SetEmail(new Email(0, emailTextSplit[0], emailTextSplit[1]));
+        button.transform.SetParent(buttonTemplate.transform.parent, false);
+
+
+        Debug.Log("2nds button");
+
+        GameObject button2 = Instantiate(buttonTemplate) as GameObject;
+        button2.SetActive(true);
+
+        button2.GetComponent<EmailButton>().SetEmail(new Email(1, emailTextSplit[2], emailTextSplit[3], "the email mentioned the A waypoint..."));
+        button2.transform.SetParent(buttonTemplate.transform.parent, false);
+
+        Debug.Log("thirds button");
+
+
+        GameObject button3 = Instantiate(buttonTemplate) as GameObject;
+        button3.SetActive(true);
+
+        button3.GetComponent<EmailButton>().SetEmail(new Email(2, emailTextSplit[4], emailTextSplit[5]));
+        button3.transform.SetParent(buttonTemplate.transform.parent, false);
+    }
+    public void GenEmail(int emailID)
+    {
+
+        flashingText.GetComponent<TextFlashScript>().Flash("NEW EMAIL");
+
+        GameObject button = Instantiate(buttonTemplate) as GameObject;
+        button.SetActive(true);
+
+        button.GetComponent<EmailButton>().SetEmail(new Email(emailID));
+        button.transform.SetParent(buttonTemplate.transform.parent, false);
+    }
+}
