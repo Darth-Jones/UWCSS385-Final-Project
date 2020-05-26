d9696b9e15589bb2896b085c10b846ca2245bd3a
lukaszbak <lukaszdbakun@gmail.com>
2020-05-25 18:55:37 -0700

    Functional KeypadSystem, Enter Button can be programmed to interract with triggers

--- Keypad_System/Assets/Keypad_System_Assets/Scripts/KeypadSystem.cs
+++ Keypad_System/Assets/Keypad_System_Assets/Scripts/KeypadSystem.cs
@@ -1,17 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
 
 public class KeypadSystem : MonoBehaviour
 {
     public int maxNumbers = 7; //The count of input numbers
-    private int[] numbers;
-    private int numberLocation = 0;
+    private int[] numbers; // actual numbers in array
+    private int numberLocation = 0; //curent location in array
+    private List<GameObject> displayNumbers; //objects for the number displays
+    private SpriteRenderer[] numberToDisplay; //spriterenderers for display
+    private List<GameObject> sortedDisplayNumbers; // sorted list of displays
+    private Sprite[] numSprites; // array of sprites to grab from
+    private bool lastReached = false; // keeps track of if max was reached
 
     // Start is called before the first frame update
     void Start()
     {
         numbers = new int[maxNumbers];
+        displayNumbers = new List<GameObject>(GameObject.FindGameObjectsWithTag("DisplayNumber"));
+        sortedDisplayNumbers = displayNumbers.OrderBy(displayNumber => displayNumber.transform.position.x).ToList();
+        numberToDisplay = new SpriteRenderer[maxNumbers];
+        numSprites =  Resources.LoadAll<Sprite>("Keypad_System_Assets/Display_Numbers");
+        for(int i = 0; i < sortedDisplayNumbers.Count; i++ ) {
+            numberToDisplay[i] = sortedDisplayNumbers[i].GetComponent<SpriteRenderer>();
+            Debug.Log(numberToDisplay[i]);
+            //Debug.Log(numSprites[i]);
+            //Debug.Log(numbers.Length);
+            
+        }
+        numSprites =  Resources.LoadAll<Sprite>("Keypad_System_Assets/Display_Numbers");
     }
 
     // Update is called once per frame
@@ -21,29 +40,55 @@ public class KeypadSystem : MonoBehaviour
             UpdateKeyPadDisplay();
     }
 
+    //Adds a number press to the Keypad
     public void ProcessNumberPress(int inputNumber)
     {
-        if(numbers.Length < maxNumbers)
-        {
-            numbers[numberLocation] = inputNumber;
-            numberLocation++;
+        if (!lastReached) {
+            if(numbers.Length <= maxNumbers)
+            {
+                
+                if ((numberLocation == maxNumbers )) {
+                    lastReached = true;
+                }
+                else {
+                    numbers[numberLocation] = inputNumber;
+                }
+                if (numberLocation < maxNumbers ) {
+                    numberLocation++;
+                    
+                } 
+            }
+            UpdateKeyPadDisplay();
         }
-        UpdateKeyPadDisplay();
     }
 
+    //updates the sprites to display
     void UpdateKeyPadDisplay()
     {
-        SpriteRenderer displayNumber = this.transform.GetChild(0).GetComponent<SpriteRenderer>();    
-        displayNumber.enabled = !displayNumber.enabled;
+        //Updates all the numbers with the sprites
+        int i = 0;
+        foreach (int number in numbers) {
+            numberToDisplay[i].sprite = numSprites[number];
+            i++;
+            Debug.Log(number);
+            lastReached = false;
+        }
     }
 
-    void ClearKeyPadDisplay()
+    // Clears the display
+    public void ClearKeyPadDisplay()
     {
-
+        numbers = new int[maxNumbers];
+        numberLocation = 0;
+        UpdateKeyPadDisplay();
+        //Debug.Log(numbers);
     }
 
-    void EnterKeyPressed()
+    //processes the number and checks if correct
+    public void EnterKeyPressed()
     {
-
+        //if (numbers == trigger.expectedNums) {
+            //  Perform Trigger
+        //}
     }
 }
