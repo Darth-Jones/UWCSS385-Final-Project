df66a446c03f029f44b3e28adebc77e71a298c6c
lukaszbak <lukaszdbakun@gmail.com>
2020-05-26 15:02:06 -0700

    Added the interactables

---
+++ Final-Project-Game/Assets/DoorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    public bool enabled = true;
+    public int[] keyCode;
+    public int firstNum;
+    public int secondNum;
+    public int thirdNum;
+    public int fourthNum;
+    public int fifthNum;
+    public int sixthNum;
+    public int seventhNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyCode = new int[] {firstNum, secondNum, thirdNum, fourthNum, fifthNum, sixthNum, seventhNum};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!enabled) {
+            Destroy(this);
+        }
+    }
+}
