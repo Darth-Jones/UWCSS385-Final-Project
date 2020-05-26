63da1a6a50fea2aae913f9efe507b5e510b3663d
Darth-Jones <55859742+Darth-Jones@users.noreply.github.com>
2020-05-25 18:48:23 -0700

    Added Scripts

---
+++ Final-Project-Game/Assets/Scripts_Interactions/InteractionObject.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+    //Interaction Options
+    public GameObject interactable;
+    public bool displayObjectOnClick = false;
+    public bool displayUIText = false;
+    public string UIText;
+    public bool createEmail = false;
+    public bool creatEvent = false;
+    public bool repeatInteraction = false;
+    private bool interactedWith = false;
+
+    
+
+    public bool requiresEventAccomplishment = false;
+    public int eventNumberRequired;
+
+    //Email Creation Options
+    public int emailID;
+    public string emailHeader;
+    public string emailBody;
+    public string emailHintText;
+
+    //Event Creation Options
+    public int eventID;
+    public int stepCount;
+    public string[] eventHintText;
+
+    //Event Completetion Options
+    public bool completesEventStep;
+    public int completeEventID;
+    public int step;
+
+
+    //Determines type of trigger by using string
+    //Trigger Types: enter, exit, and click
+    public void Triggered(string trigger)
+    {
+        
+        if (!(interactedWith))
+        {
+            interactedWith = true;
+
+            if (trigger == "enter")
+            {
+                if (displayUIText)
+                    DisplayUIText();
+
+
+            }
+            else if (trigger == "exit")
+            {
+                if (displayUIText)
+                    RemoveUIText();
+
+            }
+            else if (trigger == "click")
+            {
+                if (createEmail)
+                    CreateEmail();
+
+                if (creatEvent)
+                    CreateEvent();
+
+                if (completesEventStep)
+                    CompleteEventStep();
+            }
+        }
+
+        if (repeatInteraction)
+        {
+            interactedWith = false;
+        }
+
+    }
+
+    //Displays Text on UI object 
+    void DisplayUIText()
+    {
+        //need to implement UI Object to display
+    }
+
+    void RemoveUIText()
+    {
+
+    }
+
+    //Creates a new event for the objectives
+    void CreateEvent()
+    {
+
+    }
+
+    //Completes the given event step
+    void CompleteEventStep()
+    {
+
+    }
+
+    //Creates an Email Object
+    void CreateEmail()
+    {
+
+    }
+
+
+}
---
+++ Final-Project-Game/Assets/Scripts_Interactions/PlayerInteraction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField]
+    private bool inInteractionZone = false;
+    private GameObject interactionObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inInteractionZone)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                InteractionObject interactionScript = interactionObject.GetComponent<InteractionObject>();
+                interactionScript.Triggered("click");
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        inInteractionZone = !inInteractionZone;
+        interactionObject = collision.gameObject;
+        InteractionObject interactionScript = interactionObject.GetComponent<InteractionObject>();
+        interactionScript.Triggered("enter");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        inInteractionZone = !inInteractionZone;
+        interactionObject = collision.gameObject;
+        InteractionObject interactionScript = interactionObject.GetComponent<InteractionObject>();
+        interactionScript.Triggered("exit");
+    }
+}
