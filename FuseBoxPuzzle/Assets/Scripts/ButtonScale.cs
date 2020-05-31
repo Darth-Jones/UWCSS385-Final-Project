8399fcabf156616c49a1ed622e106234fb885ec8
Tom Blanchard <tomkb@uw.edu>
2020-05-31 15:52:42 -0700

    FuseBox Part2
    Created prfabs

--- FuseBoxPuzzle/Assets/Scripts/ButtonScale.cs
+++ FuseBoxPuzzle/Assets/Scripts/ButtonScale.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class ButtonScale : MonoBehaviour
 {
-    [SerializeField] private Vector3 scaleUp;
+    [SerializeField] private Vector3 scaleUp = new Vector3(1.2f, 1.2f, 1.2f);
     private Vector3 scaleDown;
 
     // Start is called before the first frame update
     void Start()
     {
-        //scaleUp = new Vector3(1.2f, 1.2f, 1.2f);
+        //scaleUp 
         scaleDown = new Vector3(transform.localScale.x,
             transform.localScale.y, transform.localScale.z);
     }
