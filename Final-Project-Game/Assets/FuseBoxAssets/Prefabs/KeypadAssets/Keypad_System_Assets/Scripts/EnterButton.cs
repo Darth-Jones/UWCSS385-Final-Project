393709a7c7052602c2a1fcd9d4dc6fcb1c697a03
Tom Blanchard <tomkb@uw.edu>
2020-06-01 17:53:54 -0700

    FuseBox win control
    **NOT Item transfer system

---
+++ Final-Project-Game/Assets/FuseBoxAssets/Prefabs/KeypadAssets/Keypad_System_Assets/Scripts/EnterButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterButton : MonoBehaviour
+{
+    private KeypadSystem keypadSystem;
+
+    private void Start()
+    {
+        keypadSystem = gameObject.GetComponentInParent < KeypadSystem > ();
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("click Enter");
+            keypadSystem.EnterKeyPressed();
+        }     
+    }
+}
