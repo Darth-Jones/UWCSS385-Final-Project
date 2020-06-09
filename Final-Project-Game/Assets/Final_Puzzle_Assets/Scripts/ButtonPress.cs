60bb9c107fc3f41deb690d1e299b762705427a8d
Tom Blanchard <tomkb@uw.edu>
2020-06-08 18:13:09 -0700

    Final Puzzle
    Button press in a certain sequence to solve the puzzle
    - Solution is currently: A+, Endogeneous, N12N19 --> submit

---
+++ Final-Project-Game/Assets/Final_Puzzle_Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Color OGColor;
+    public Color changeColor;
+    private bool buttonPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        
+    }
+
+    public void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!buttonPressed)
+            {
+                sr.material.color = changeColor;
+                buttonPressed = true;
+            }
+            else {
+                Invoke("ChangeColorBack", 0.1f);
+            }
+           
+        }
+    }
+
+    private void ChangeColorBack()
+    {
+
+            sr.material.color = OGColor;
+            buttonPressed = false;
+
+    }
+}
---
+++ Final-Project-Game/Assets/Final_Puzzle_Assets/Scripts/FinalPuzzleWinControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FinalPuzzleWinControl : MonoBehaviour
+{
+    public GameObject emailSystem;
+    public string[] hintText = { "Item transfer system beeped: it's now up and running" };
+    public int eventID;
+    private bool called = false;
+    private int[] winningSequence = new int[] { 0, 8, 16 };
+    private List<int> enteredSequence;
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        enteredSequence = new List<int>();
+    }
+
+    public void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+
+            if (!called)
+
+            {
+                if (winningSequence.SequenceEqual(enteredSequence))
+                {
+                    Debug.Log("SOLVED");
+                    //called = true;
+                    //emailSystem.GetComponent<EventLog>().CreateEvent(eventID, 1, hintText); //using event 10
+                    //emailSystem.GetComponent<EventLog>().StepCompleted(eventID, 0);
+                }
+                else {
+                    Debug.Log("Not Solved");
+                    enteredSequence = new List<int>();
+                }
+            }
+        }
+    }
+
+    public void AddToWinSequence(int numToAdd) {
+        enteredSequence.Add(numToAdd);
+    }
+}
